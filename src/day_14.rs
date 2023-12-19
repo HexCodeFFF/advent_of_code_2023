d04e88ebcb69227421f2fba1b72c203efaf72da4
Melody <reticivis@gmail.com>
2023-12-19 00:11:34 -0600

    bleh

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,5 +1,23 @@
 const INPUT: &str = include_str!("../inputs/day_14.txt");
 
-pub fn star_1() {}
+enum RockType {
+    Rounded,
+    Cube,
+}
+pub fn star_1() {
+    let mut dish: Vec<Vec<Option<RockType>>> = INPUT
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|ch| match ch {
+                    '.' => None,
+                    '#' => Some(RockType::Cube),
+                    'O' => Some(RockType::Rounded),
+                    r => panic!("unknown rock {r}"),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+}
 
 pub fn star_2() {}
